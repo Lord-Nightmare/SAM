c49d44ebdd7d26c981579a97987fd5d04cac2f60
Lord-Nightmare <Lord-Nightmare@users.noreply.github.com>
2026-02-09 16:23:44 -0500

    rename variables in reciter to fix compile

--- src/reciter.c
+++ src/reciter.c
@@ -4,41 +4,41 @@
 #include "ReciterTabs.h"
 #include "debug.h"
 
-unsigned char A, X, Y;
+unsigned char Ar, Xr, Yr;
 extern int debug;
 
 static unsigned char inputtemp[256];   // secure copy of input tab36096
 
 void Code37055(unsigned char mem59)
 {
-	X = mem59;
-	X--;
-	A = inputtemp[X];
-	Y = A;
-	A = tab36376[Y];
+	Xr = mem59;
+	Xr--;
+	Ar = inputtemp[Xr];
+	Yr = Ar;
+	Ar = tab36376[Yr];
 	return;
 }
 
 void Code37066(unsigned char mem58)
 {
-	X = mem58;
-	X++;
-	A = inputtemp[X];
-	Y = A;
-	A = tab36376[Y];
+	Xr = mem58;
+	Xr++;
+	Ar = inputtemp[Xr];
+	Yr = Ar;
+	Ar = tab36376[Yr];
 }
 
-unsigned char GetRuleByte(unsigned short mem62, unsigned char Y)
+unsigned char GetRuleByte(unsigned short mem62, unsigned char Yr)
 {
 	unsigned int address = mem62;
 	
 	if (mem62 >= 37541) 
 	{
 		address -= 37541;
-		return rules2[address+Y];
+		return rules2[address+Yr];
 	}
 	address -= 32000;
-	return rules[address+Y];
+	return rules[address+Yr];
 }
 
 int TextToPhonemes(char *input) // Code36484
@@ -62,28 +62,28 @@ int TextToPhonemes(char *input) // Code36484
 
 	// secure copy of input
 	// because input will be overwritten by phonemes
-	X = 1;
-	Y = 0;
+	Xr = 1;
+	Yr = 0;
 	do
 	{
 		//pos36499:
-		A = input[Y] & 127;
-		if ( A >= 112) A = A & 95;
-		else if ( A >= 96) A = A & 79;
+		Ar = input[Yr] & 127;
+		if ( Ar >= 112) Ar = Ar & 95;
+		else if ( Ar >= 96) Ar = Ar & 79;
 		
-		inputtemp[X] = A;
-		X++;
-		Y++;
-	} while (Y != 255);
+		inputtemp[Xr] = Ar;
+		Xr++;
+		Yr++;
+	} while (Yr != 255);
 
 
-	X = 255;
-	inputtemp[X] = 27;
+	Xr = 255;
+	inputtemp[Xr] = 27;
 	mem61 = 255;
 
 
 pos36550:
-	A = 255;
+	Ar = 255;
 	mem56 = 255;
 
 
@@ -91,53 +91,53 @@ pos36554:
 	while(1)
 	{
 		mem61++;
-		X = mem61;
-		A = inputtemp[X];
-		mem64 = A;
-		if (A == '[')
+		Xr = mem61;
+		Ar = inputtemp[Xr];
+		mem64 = Ar;
+		if (Ar == '[')
 		{
 			mem56++;
-			X = mem56;
-			A = 155;
-			input[X] = 155;
+			Xr = mem56;
+			Ar = 155;
+			input[Xr] = 155;
 			//goto pos36542;
 			//			Code39771(); 	//Code39777();
 			return 1;
 		}
 
 		//pos36579:
-		if (A != '.') break;
-		X++;
-		Y = inputtemp[X];
-		A = tab36376[Y] & 1;
-		if(A != 0) break;
+		if (Ar != '.') break;
+		Xr++;
+		Yr = inputtemp[Xr];
+		Ar = tab36376[Yr] & 1;
+		if(Ar != 0) break;
 		mem56++;
-		X = mem56;
-		A = '.';
-		input[X] = '.';
+		Xr = mem56;
+		Ar = '.';
+		input[Xr] = '.';
 	} //while
 
 
 	//pos36607:
-	A = mem64;
-	Y = A;
-	A = tab36376[A];
-	mem57 = A;
-	if((A&2) != 0)
+	Ar = mem64;
+	Yr = Ar;
+	Ar = tab36376[Ar];
+	mem57 = Ar;
+	if((Ar&2) != 0)
 	{
 		mem62 = 37541;
 		goto pos36700;
 	}
 
 	//pos36630:
-	A = mem57;
-	if(A != 0) goto pos36677;
-	A = 32;
-	inputtemp[X] = ' ';
+	Ar = mem57;
+	if(Ar != 0) goto pos36677;
+	Ar = 32;
+	inputtemp[Xr] = ' ';
 	mem56++;
-	X = mem56;
-	if (X > 120) goto pos36654;
-	input[X] = A;
+	Xr = mem56;
+	if (Xr > 120) goto pos36654;
+	input[Xr] = Ar;
 	goto pos36554;
 
 	// -----
@@ -145,10 +145,10 @@ pos36554:
 	//36653 is unknown. Contains position
 
 pos36654:
-	input[X] = 155;
-	A = mem61;
-	mem36653 = A;
-	//	mem29 = A; // not used
+	input[Xr] = 155;
+	Ar = mem61;
+	mem36653 = Ar;
+	//	mem29 = Ar; // not used
 	//	Code36538(); das ist eigentlich
 	return 1;
 	//Code39771();
@@ -157,16 +157,16 @@ pos36654:
 	goto pos36550;
 
 pos36677:
-	A = mem57 & 128;
-	if(A == 0)
+	Ar = mem57 & 128;
+	if(Ar == 0)
 	{
 		//36683: BRK
 		return 0;
 	}
 
 	// go to the right rules for this character.
-	X = mem64 - 'A';
-	mem62 = tab37489[X] | (tab37515[X]<<8);
+	Xr = mem64 - 'A';
+	mem62 = tab37489[Xr] | (tab37515[Xr]<<8);
 
 	// -------------------------------------
 	// go to next rule
@@ -175,95 +175,95 @@ pos36677:
 pos36700:
 
 	// find next rule
-	Y = 0;
+	Yr = 0;
 	do
 	{
 		mem62 += 1;
-		A = GetRuleByte(mem62, Y);
-	} while ((A & 128) == 0);
-	Y++;
+		Ar = GetRuleByte(mem62, Yr);
+	} while ((Ar & 128) == 0);
+	Yr++;
 
 	//pos36720:
 	// find '('
 	while(1)
 	{
-		A = GetRuleByte(mem62, Y);
-		if (A == '(') break;
-		Y++;
+		Ar = GetRuleByte(mem62, Yr);
+		if (Ar == '(') break;
+		Yr++;
 	}
-	mem66 = Y;
+	mem66 = Yr;
 
 	//pos36732:
 	// find ')'
 	do
 	{
-		Y++;
-		A = GetRuleByte(mem62, Y);
-	} while(A != ')');
-	mem65 = Y;
+		Yr++;
+		Ar = GetRuleByte(mem62, Yr);
+	} while(Ar != ')');
+	mem65 = Yr;
 
 	//pos36741:
 	// find '='
 	do
 	{
-		Y++;
-		A = GetRuleByte(mem62, Y);
-		A = A & 127;
-	} while (A != '=');
-	mem64 = Y;
+		Yr++;
+		Ar = GetRuleByte(mem62, Yr);
+		Ar = Ar & 127;
+	} while (Ar != '=');
+	mem64 = Yr;
 
-	X = mem61;
-	mem60 = X;
+	Xr = mem61;
+	mem60 = Xr;
 
 	// compare the string within the bracket
-	Y = mem66;
-	Y++;
+	Yr = mem66;
+	Yr++;
 	//pos36759:
 	while(1)
 	{
-		mem57 = inputtemp[X];
-		A = GetRuleByte(mem62, Y);
-		if (A != mem57) goto pos36700;
-		Y++;
-		if(Y == mem65) break;
-		X++;
-		mem60 = X;
+		mem57 = inputtemp[Xr];
+		Ar = GetRuleByte(mem62, Yr);
+		if (Ar != mem57) goto pos36700;
+		Yr++;
+		if(Yr == mem65) break;
+		Xr++;
+		mem60 = Xr;
 	}
 
 // the string in the bracket is correct
 
 //pos36787:
-	A = mem61;
+	Ar = mem61;
 	mem59 = mem61;
 
 pos36791:
 	while(1)
 	{
 		mem66--;
-		Y = mem66;
-		A = GetRuleByte(mem62, Y);
-		mem57 = A;
+		Yr = mem66;
+		Ar = GetRuleByte(mem62, Yr);
+		mem57 = Ar;
 		//36800: BPL 36805
-		if ((A & 128) != 0) goto pos37180;
-		X = A & 127;
-		A = tab36376[X] & 128;
-		if (A == 0) break;
-		X = mem59-1;
-		A = inputtemp[X];
-		if (A != mem57) goto pos36700;
-		mem59 = X;
+		if ((Ar & 128) != 0) goto pos37180;
+		Xr = Ar & 127;
+		Ar = tab36376[Xr] & 128;
+		if (Ar == 0) break;
+		Xr = mem59-1;
+		Ar = inputtemp[Xr];
+		if (Ar != mem57) goto pos36700;
+		mem59 = Xr;
 	}
 
 //pos36833:
-	A = mem57;
-	if (A == ' ') goto pos36895;
-	if (A == '#') goto pos36910;
-	if (A == '.') goto pos36920;
-	if (A == '&') goto pos36935;
-	if (A == '@') goto pos36967;
-	if (A == '^') goto pos37004;
-	if (A == '+') goto pos37019;
-	if (A == ':') goto pos37040;
+	Ar = mem57;
+	if (Ar == ' ') goto pos36895;
+	if (Ar == '#') goto pos36910;
+	if (Ar == '.') goto pos36920;
+	if (Ar == '&') goto pos36935;
+	if (Ar == '@') goto pos36967;
+	if (Ar == '^') goto pos37004;
+	if (Ar == '+') goto pos37019;
+	if (Ar == ':') goto pos37040;
 	//	Code42041();    //Error
 	//36894: BRK
 	return 0;
@@ -272,18 +272,18 @@ pos36791:
 
 pos36895:
 	Code37055(mem59);
-	A = A & 128;
-	if(A != 0) goto pos36700;
+	Ar = Ar & 128;
+	if(Ar != 0) goto pos36700;
 pos36905:
-	mem59 = X;
+	mem59 = Xr;
 	goto pos36791;
 
 	// --------------
 
 pos36910:
 	Code37055(mem59);
-	A = A & 64;
-	if(A != 0) goto pos36905;
+	Ar = Ar & 64;
+	if(Ar != 0) goto pos36905;
 	goto pos36700;
 
 	// --------------
@@ -291,35 +291,35 @@ pos36910:
 
 pos36920:
 	Code37055(mem59);
-	A = A & 8;
-	if(A == 0) goto pos36700;
+	Ar = Ar & 8;
+	if(Ar == 0) goto pos36700;
 pos36930:
-	mem59 = X;
+	mem59 = Xr;
 	goto pos36791;
 
 	// --------------
 
 pos36935:
 	Code37055(mem59);
-	A = A & 16;
-	if(A != 0) goto pos36930;
-	A = inputtemp[X];
-	if (A != 72) goto pos36700;
-	X--;
-	A = inputtemp[X];
-	if ((A == 67) || (A == 83)) goto pos36930;
+	Ar = Ar & 16;
+	if(Ar != 0) goto pos36930;
+	Ar = inputtemp[Xr];
+	if (Ar != 72) goto pos36700;
+	Xr--;
+	Ar = inputtemp[Xr];
+	if ((Ar == 67) || (Ar == 83)) goto pos36930;
 	goto pos36700;
 
 	// --------------
 
 pos36967:
 	Code37055(mem59);
-	A = A & 4;
-	if(A != 0) goto pos36930;
-	A = inputtemp[X];
-	if (A != 72) goto pos36700;
-	if ((A != 84) && (A != 67) && (A != 83)) goto pos36700;
-	mem59 = X;
+	Ar = Ar & 4;
+	if(Ar != 0) goto pos36930;
+	Ar = inputtemp[Xr];
+	if (Ar != 72) goto pos36700;
+	if ((Ar != 84) && (Ar != 67) && (Ar != 83)) goto pos36700;
+	mem59 = Xr;
 	goto pos36791;
 
 	// --------------
@@ -327,74 +327,74 @@ pos36967:
 
 pos37004:
 	Code37055(mem59);
-	A = A & 32;
-	if(A == 0) goto pos36700;
+	Ar = Ar & 32;
+	if(Ar == 0) goto pos36700;
 
 pos37014:
-	mem59 = X;
+	mem59 = Xr;
 	goto pos36791;
 
 	// --------------
 
 pos37019:
-	X = mem59;
-	X--;
-	A = inputtemp[X];
-	if ((A == 'E') || (A == 'I') || (A == 'Y')) goto pos37014;
+	Xr = mem59;
+	Xr--;
+	Ar = inputtemp[Xr];
+	if ((Ar == 'E') || (Ar == 'I') || (Ar == 'Y')) goto pos37014;
 	goto pos36700;
 	// --------------
 
 pos37040:
 	Code37055(mem59);
-	A = A & 32;
-	if(A == 0) goto pos36791;
-	mem59 = X;
+	Ar = Ar & 32;
+	if(Ar == 0) goto pos36791;
+	mem59 = Xr;
 	goto pos37040;
 
 //---------------------------------------
 
 
 pos37077:
-	X = mem58+1;
-	A = inputtemp[X];
-	if (A != 'E') goto pos37157;
-	X++;
-	Y = inputtemp[X];
-	X--;
-	A = tab36376[Y] & 128;
-	if(A == 0) goto pos37108;
-	X++;
-	A = inputtemp[X];
-	if (A != 'R') goto pos37113;
+	Xr = mem58+1;
+	Ar = inputtemp[Xr];
+	if (Ar != 'E') goto pos37157;
+	Xr++;
+	Yr = inputtemp[Xr];
+	Xr--;
+	Ar = tab36376[Yr] & 128;
+	if(Ar == 0) goto pos37108;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if (Ar != 'R') goto pos37113;
 pos37108:
-	mem58 = X;
+	mem58 = Xr;
 	goto pos37184;
 pos37113:
-	if ((A == 83) || (A == 68)) goto pos37108;  // 'S' 'D'
-	if (A != 76) goto pos37135; // 'L'
-	X++;
-	A = inputtemp[X];
-	if (A != 89) goto pos36700;
+	if ((Ar == 83) || (Ar == 68)) goto pos37108;  // 'S' 'D'
+	if (Ar != 76) goto pos37135; // 'L'
+	Xr++;
+	Ar = inputtemp[Xr];
+	if (Ar != 89) goto pos36700;
 	goto pos37108;
 	
 pos37135:
-	if (A != 70) goto pos36700;
-	X++;
-	A = inputtemp[X];
-	if (A != 85) goto pos36700;
-	X++;
-	A = inputtemp[X];
-	if (A == 76) goto pos37108;
+	if (Ar != 70) goto pos36700;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if (Ar != 85) goto pos36700;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if (Ar == 76) goto pos37108;
 	goto pos36700;
 
 pos37157:
-	if (A != 73) goto pos36700;
-	X++;
-	A = inputtemp[X];
-	if (A != 78) goto pos36700;
-	X++;
-	A = inputtemp[X];
-	if (A == 71) goto pos37108;
+	if (Ar != 73) goto pos36700;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if (Ar != 78) goto pos36700;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if (Ar == 71) goto pos37108;
 	//pos37177:
 	goto pos36700;
 
@@ -402,38 +402,38 @@ pos37157:
 
 pos37180:
 
-	A = mem60;
-	mem58 = A;
+	Ar = mem60;
+	mem58 = Ar;
 
 pos37184:
-	Y = mem65 + 1;
+	Yr = mem65 + 1;
 
 	//37187: CPY 64
 	//	if(? != 0) goto pos37194;
-	if(Y == mem64) goto pos37455;
-	mem65 = Y;
+	if(Yr == mem64) goto pos37455;
+	mem65 = Yr;
 	//37196: LDA (62),y
-	A = GetRuleByte(mem62, Y);
-	mem57 = A;
-	X = A;
-	A = tab36376[X] & 128;
-	if(A == 0) goto pos37226;
-	X = mem58+1;
-	A = inputtemp[X];
-	if (A != mem57) goto pos36700;
-	mem58 = X;
+	Ar = GetRuleByte(mem62, Yr);
+	mem57 = Ar;
+	Xr = Ar;
+	Ar = tab36376[Xr] & 128;
+	if(Ar == 0) goto pos37226;
+	Xr = mem58+1;
+	Ar = inputtemp[Xr];
+	if (Ar != mem57) goto pos36700;
+	mem58 = Xr;
 	goto pos37184;
 pos37226:
-	A = mem57;
-	if (A == 32) goto pos37295;   // ' '
-	if (A == 35) goto pos37310;   // '#'
-	if (A == 46) goto pos37320;   // '.'
-	if (A == 38) goto pos37335;   // '&'
-	if (A == 64) goto pos37367;   // ''
-	if (A == 94) goto pos37404;   // ''
-	if (A == 43) goto pos37419;   // '+'
-	if (A == 58) goto pos37440;   // ':'
-	if (A == 37) goto pos37077;   // '%'
+	Ar = mem57;
+	if (Ar == 32) goto pos37295;   // ' '
+	if (Ar == 35) goto pos37310;   // '#'
+	if (Ar == 46) goto pos37320;   // '.'
+	if (Ar == 38) goto pos37335;   // '&'
+	if (Ar == 64) goto pos37367;   // ''
+	if (Ar == 94) goto pos37404;   // ''
+	if (Ar == 43) goto pos37419;   // '+'
+	if (Ar == 58) goto pos37440;   // ':'
+	if (Ar == 37) goto pos37077;   // '%'
 	//pos37291:
 	//	Code42041(); //Error
 	//37294: BRK
@@ -442,18 +442,18 @@ pos37226:
 	// --------------
 pos37295:
 	Code37066(mem58);
-	A = A & 128;
-	if(A != 0) goto pos36700;
+	Ar = Ar & 128;
+	if(Ar != 0) goto pos36700;
 pos37305:
-	mem58 = X;
+	mem58 = Xr;
 	goto pos37184;
 
 	// --------------
 
 pos37310:
 	Code37066(mem58);
-	A = A & 64;
-	if(A != 0) goto pos37305;
+	Ar = Ar & 64;
+	if(Ar != 0) goto pos37305;
 	goto pos36700;
 
 	// --------------
@@ -461,24 +461,24 @@ pos37310:
 
 pos37320:
 	Code37066(mem58);
-	A = A & 8;
-	if(A == 0) goto pos36700;
+	Ar = Ar & 8;
+	if(Ar == 0) goto pos36700;
 
 pos37330:
-	mem58 = X;
+	mem58 = Xr;
 	goto pos37184;
 
 	// --------------
 
 pos37335:
 	Code37066(mem58);
-	A = A & 16;
-	if(A != 0) goto pos37330;
-	A = inputtemp[X];
-	if (A != 72) goto pos36700;
-	X++;
-	A = inputtemp[X];
-	if ((A == 67) || (A == 83)) goto pos37330;
+	Ar = Ar & 16;
+	if(Ar != 0) goto pos37330;
+	Ar = inputtemp[Xr];
+	if (Ar != 72) goto pos36700;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if ((Ar == 67) || (Ar == 83)) goto pos37330;
 	goto pos36700;
 
 	// --------------
@@ -486,31 +486,31 @@ pos37335:
 
 pos37367:
 	Code37066(mem58);
-	A = A & 4;
-	if(A != 0) goto pos37330;
-	A = inputtemp[X];
-	if (A != 72) goto pos36700;
-	if ((A != 84) && (A != 67) && (A != 83)) goto pos36700;
-	mem58 = X;
+	Ar = Ar & 4;
+	if(Ar != 0) goto pos37330;
+	Ar = inputtemp[Xr];
+	if (Ar != 72) goto pos36700;
+	if ((Ar != 84) && (Ar != 67) && (Ar != 83)) goto pos36700;
+	mem58 = Xr;
 	goto pos37184;
 
 	// --------------
 
 pos37404:
 	Code37066(mem58);
-	A = A & 32;
-	if(A == 0) goto pos36700;
+	Ar = Ar & 32;
+	if(Ar == 0) goto pos36700;
 pos37414:
-	mem58 = X;
+	mem58 = Xr;
 	goto pos37184;
 
 	// --------------
 	
 pos37419:
-	X = mem58;
-	X++;
-	A = inputtemp[X];
-	if ((A == 69) || (A == 73) || (A == 89)) goto pos37414;
+	Xr = mem58;
+	Xr++;
+	Ar = inputtemp[Xr];
+	if ((Ar == 69) || (Ar == 73) || (Ar == 89)) goto pos37414;
 	goto pos36700;
 
 // ----------------------
@@ -518,12 +518,12 @@ pos37419:
 pos37440:
 
 	Code37066(mem58);
-	A = A & 32;
-	if(A == 0) goto pos37184;
-	mem58 = X;
+	Ar = Ar & 32;
+	if(Ar == 0) goto pos37184;
+	mem58 = Xr;
 	goto pos37440;
 pos37455:
-	Y = mem64;
+	Yr = mem64;
 	mem61 = mem60;
 
 	if (debug)
@@ -531,14 +531,14 @@ pos37455:
 
 pos37461:
 	//37461: LDA (62),y
-	A = GetRuleByte(mem62, Y);
-	mem57 = A;
-	A = A & 127;
-	if (A != '=')
+	Ar = GetRuleByte(mem62, Yr);
+	mem57 = Ar;
+	Ar = Ar & 127;
+	if (Ar != '=')
 	{
 		mem56++;
-		X = mem56;
-		input[X] = A;
+		Xr = mem56;
+		input[Xr] = Ar;
 	}
 
 	//37478: BIT 57
@@ -546,7 +546,7 @@ pos37461:
 	if ((mem57 & 128) == 0) goto pos37485; //???
 	goto pos36554;
 pos37485:
-	Y++;
+	Yr++;
 	goto pos37461;
 }
 
